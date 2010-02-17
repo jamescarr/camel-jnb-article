9f56365659fbd06b540bf90f125683a1e5eb51bf
James Carr <jrcarr2@jrcarr2-laptop.(none)>
2010-02-16 23:00:30 -0600

    made things common between pojo and seda example

---
+++ seda-example/src/main/java/com/ociweb/example/domain/SprocketWarehouse.java
@@ -0,0 +1,9 @@
+package com.ociweb.example.domain;
+
+public class SprocketWarehouse implements Warehouse {
+	private static final String MESSAGE = "Recieved Order for %d %s that will ship on %s.%n";
+	public void shipProduct(ShippingOrder order) {
+		System.out.printf(MESSAGE,order.getQuantity(),order.getProduct(), order.getExpectedShipDate().toString("MM/dd/yyyy"));
+	}
+
+}
